adcea2d0a2060fcddbcb6f98265316ed588fad13
Derek Sharp <fulminstudios@gmail.com>
2019-06-22 22:08:34 -0600

    Header rows starting to get brought in

--- wp-content/plugins/uxi-migration/migrator/migrations/uxi-migration-do-header-rows.php
+++ wp-content/plugins/uxi-migration/migrator/migrations/uxi-migration-do-header-rows.php
@@ -5,26 +5,58 @@ function uxi_do_header_rows($dom) {
 
 		$xpath = new DOMXpath($dom);
 
-		update_field('block',array(),'option');
-		//var_dump(get_field('block','option'));
-		$field_array = array();
+		function uxi_get_items($dom, $xpath, $query_array, $query_index, $layout_array, $layout_index, $fields = array() ) {
+			if ($query_index < count($query_array)) {
+				foreach($xpath->query($query_array[$query_index]) as $element) {
 
-		foreach($xpath->query("//*[@uxi-header]//*[@data-layout]") as $element) {
-			$element_attributes = array();
-			if ($element->hasAttributes()) {
-				foreach($element->attributes as $attr) {
-					array_push($element_attributes,array(
-						'attribute_name' => $attr->nodeName,
-						'attribute_value' => $attr->nodeValue,
+					$id="";
+					$class="";
+					if ($element->hasAttributes()) {
+						$id = $element->attributes->getNamedItem('id')->value;
+						$class = $element->attributes->getNamedItem('class')->value;
+					}
+
+					array_push($fields,array(
+						'acf_fc_layout' => $layout_array[$layout_index],
+						'id' => $id,
+						'class' => $class
 					));
+
+					$elementHTML = $dom->saveHTML($element);
+					$elementDom = new DOMDocument();
+					@$elementDom->loadHTML($elementHTML);
+					$elementXpath = new DOMXpath($elementDom);
+
+					$fields = uxi_get_items($elementDom, $elementXpath, $query_array, $query_index + 1, $layout_array, $layout_index + 1, $fields);
 				}
 			}
-			//var_dump($element->attributes);
-			array_push($field_array,array(
-				'acf_fc_layout' => 'row',
-				'attributes' => $element_attributes,
-			));
+			return $fields;
 		}
+
+		update_field('block',array(),'option');
+
+		$query_array = array(
+			'//*[@uxi-header]//*[@data-layout]',
+			'//*[@class="container"]/*[@class="container-inner"]/*[@class="row"]/div'
+		);
+
+		$layout_array = array(
+			'row',
+			'grid_item'
+		);
+
+		$field_array = uxi_get_items(
+			$dom,
+			$xpath,
+			$query_array,
+			0,
+			$layout_array,
+			0
+		);
+
+		var_dump($field_array);
+
 		update_field('block',$field_array,'option');
+
 	}
 }
