0a950c6ab6b507a5d178c5b600e10cf44e90c13a
Derek Sharp <fulminstudios@gmail.com>
2019-06-23 11:03:46 -0600

    Navigation menus dynamically populated in widget

--- wp-content/themes/mad-uxi-migrator/inc/core.php
+++ wp-content/themes/mad-uxi-migrator/inc/core.php
@@ -1,1 +1,26 @@
 <?php
+
+function mad_get_all_wordpress_menus(){
+	return get_terms( 'nav_menu', array( 'hide_empty' => false ) ); 
+}
+
+function mad_populate_menus( $field ) {
+    
+    // reset choices
+    $field['choices'] = array();
+        
+        // while has rows
+        foreach(mad_get_all_wordpress_menus() as $menu) {
+            
+            // append to choices
+            $field['choices'][$menu->slug] = $menu->name;
+            
+        }
+
+
+    // return the field
+    return $field;
+    
+}
+
+add_filter('acf/load_field/name=widget_uxi_menu', 'mad_populate_menus');
