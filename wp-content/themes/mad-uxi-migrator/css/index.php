f10b6e40c9d064ef11bc833a6ef20a9825e0f327
Derek Sharp <fulminstudios@gmail.com>
2019-06-24 22:52:11 -0600

    Theme CSS enqueue file checks if files enqueued are CSS files

--- wp-content/themes/mad-uxi-migrator/css/index.php
+++ wp-content/themes/mad-uxi-migrator/css/index.php
@@ -4,7 +4,7 @@ function enqueue_uxi_styles() {
 	foreach (scandir(get_stylesheet_directory().'/css') as $css) {
 		$path = get_stylesheet_directory().'/css/'.$css;
 		$url = get_stylesheet_directory_uri().'/css/'.$css;
-		if (is_file($path)) {
+		if (is_file($path) && strpos($path,'.css') > -1) {
 			$name = str_replace('.','-',$css);
 			wp_enqueue_style($name,$url);
 		}
