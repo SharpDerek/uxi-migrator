78b6a6fca8d6ed306c857b9790f0e8934fe939a4
Derek Sharp <fulminstudios@gmail.com>
2019-06-25 20:39:22 -0600

    Added followlocation = true to curl request function

--- wp-content/plugins/uxi-migration/migrator/functions/uxi-function-curl.php
+++ wp-content/plugins/uxi-migration/migrator/functions/uxi-function-curl.php
@@ -13,6 +13,7 @@ function uxi_curl($input, $request = "GET") {
 	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 	  CURLOPT_CUSTOMREQUEST => $request,
 	  CURLOPT_POSTFIELDS => "",
+	  CURLOPT_FOLLOWLOCATION => true,
 	));
 
 	$response = curl_exec($curl);
