41c012fa8b2aa71b375f2cf62f234a7450444989
Derek Sharp <fulminstudios@gmail.com>
2019-07-02 22:30:42 -0600

    Nav Menus bring in proper classes, Menu matching still broken apparently

--- wp-content/plugins/uxi-migration/migrator/migrations/widgets/widget_uxi_navigation.php
+++ wp-content/plugins/uxi-migration/migrator/migrations/widgets/widget_uxi_navigation.php
@@ -6,10 +6,19 @@ if (!UXI_ITEM) {
 
 $menu = uxi_menu_match($dom);
 
+$menu_class = "nav";
+
+foreach($xpath->query('//nav//ul[contains(@class,"nav")]') as $nav_list) {
+	if ($nav_list->hasAttribute('class')) {
+		$menu_class = $nav_list->attributes->getNamedItem('class')->value;
+	}
+}
+
 array_push($fields,array(
 	'acf_fc_layout' => $widget_layout,
 	'id' => $id,
 	'class' => $class,
-	'widget_uxi_menu' => $menu
+	'widget_uxi_menu' => $menu,
+	'menu_classes' => $menu_class
 ));
 uxi_print('<i>'.$widget_layout.'</i> created. id: "'.$id.'", class: "'.$class.'", menu: "'.$menu.'"',"sub");
--- wp-content/themes/mad-uxi-migrator/layout/fields/widget_uxi_navigation.php
+++ wp-content/themes/mad-uxi-migrator/layout/fields/widget_uxi_navigation.php
@@ -3,6 +3,7 @@
 	$class = get_sub_field('class');
 	$id = get_sub_field('id');
 	$menu = get_sub_field('widget_uxi_menu');
+	$menu_class = get_sub_field('menu_classes');
 ?>
 
 <div uxi-widget="widget_uxi_navigation" id="<?php echo $id; ?>" class="<?php echo $class; ?>">
@@ -10,7 +11,7 @@
 		<nav class="navbar" role="navigation">
 			<?php wp_nav_menu(array (
 				'menu' => $menu,
-				'menu_class' => 'nav navbar-nav navbar-center',
+				'menu_class' => $menu_class,
 				'container_class' => 'navbar-container',
 				'walker' => new UXi_Nav_Walker()
 			)); ?>
