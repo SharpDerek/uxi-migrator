2e52dbf9b278ff676ca9bc2405fc0e5503453080
Derek Sharp <derek.sharp@madwiremedia.com>
2019-07-15 15:58:52 -0600

    Started Blog Categories widget

--- wp-content/plugins/uxi-migration/migrator/migrations/widgets/uxi_widget_search.php
+++ wp-content/plugins/uxi-migration/migrator/migrations/widgets/uxi_widget_search.php
@@ -1,5 +1,9 @@
 <?php
 
+if (!UXI_ITEM) {
+	return;
+}
+
 foreach ($xpath->query('//form[@role="search"]//input[@type="search"]') as $search_input) {
 	if ($search_input->hasAttribute('placeholder')) {
 		$placeholder = $search_input->attributes->getNamedItem('placeholder')->value;
