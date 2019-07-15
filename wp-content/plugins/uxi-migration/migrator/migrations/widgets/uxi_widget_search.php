6749f77ad5cf8d8f968ed6a2da3d76d5a1023c71
Derek Sharp <derek.sharp@madwiremedia.com>
2019-07-15 15:09:55 -0600

    Search form widget created

---
+++ wp-content/plugins/uxi-migration/migrator/migrations/widgets/uxi_widget_search.php
@@ -0,0 +1,15 @@
+<?php
+
+foreach ($xpath->query('//form[@role="search"]//input[@type="search"]') as $search_input) {
+	if ($search_input->hasAttribute('placeholder')) {
+		$placeholder = $search_input->attributes->getNamedItem('placeholder')->value;
+	}
+}
+
+array_push($fields,array(
+	'acf_fc_layout' => $widget_layout,
+	'id' => $id,
+	'class' => $class,
+	'placeholder' => $placeholder
+));
+uxi_print('<i>'.$widget_layout.'</i> created. id: "'.$id.'", class: "'.$class.'", placeholder: "'.$placeholder.'"');
---
+++ wp-content/themes/mad-uxi-migrator/layout/fields/uxi_widget_search.php
@@ -0,0 +1,20 @@
+<?php
+	$class = get_sub_field('class');
+	$id = get_sub_field('id');
+	$placeholder = get_sub_field('placeholder');
+?>
+<div uxi-widget="uxi_widget_search" id="<?php echo $id; ?>" class="<?php echo $class; ?>">
+	<div class="content">
+		<form role="search" method="get" class="search-form search-form-block" action="/">
+            <label class="sr-only" for="uxi-search-2">Search for:</label>
+            <div class="clearfix">
+               <div class="search-form-input">
+                   <input id="uxi-search-2" type="search" value="" name="s" placeholder="<?php echo $placeholder; ?>">
+               </div>
+                <div class="search-form-button">
+                    <button type="submit" class="button button-default"><span class="icon-uxis-search" aria-hidden="true"></span><span class="sr-only">Search</span></button>
+                </div>
+            </div>
+        </form>
+	</div>
+</div>
