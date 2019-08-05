9ea9f4102fe320a2f111212d399c08da1e3d56e9
Derek Sharp <derek.sharp@madwiremedia.com>
2019-08-05 13:15:06 -0600

    Dynamic Title brought in

--- wp-content/themes/mad-uxi-migrator/templates/loop-page.php
+++ wp-content/themes/mad-uxi-migrator/templates/loop-page.php
@@ -1,11 +1,6 @@
 <?php /* Start loop */ ?>
 <?php while (have_posts()) : the_post(); ?>
-  
-  <?php get_template_part('templates/page-header'); ?> 
 
   <?php the_content(); ?>
   
-  <?php wp_link_pages(array('before' => '<nav class="pagination-nav clearfix"><h2 class="visuallyhidden">'.__('Pagination Navigation', 'mad').'</h2>' . __('Pages:', 'mad'), 'after' => '</nav>' )); ?>
-
-<?php /* End loop */ ?>
 <?php endwhile; wp_reset_query(); ?>
