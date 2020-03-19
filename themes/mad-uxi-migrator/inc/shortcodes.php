da701bf5eedcb0beafc66b1d35b0ab418775c8b0
SharpDerek <dsharp640@comcast.net>
2020-03-19 14:13:30 -0600

    Register Gallery Shortcode

--- themes/mad-uxi-migrator/inc/shortcodes.php
+++ themes/mad-uxi-migrator/inc/shortcodes.php
@@ -5,7 +5,7 @@ define("UXI_SHORTCODES_PATH",get_stylesheet_directory().'/inc/shortcodes/');
 //require_once(UXI_SHORTCODES_PATH.'address.php');
 require_once(UXI_SHORTCODES_PATH.'button.php');
 require_once(UXI_SHORTCODES_PATH.'columns.php');
-//require_once(UXI_SHORTCODES_PATH.'gallery.php');
+require_once(UXI_SHORTCODES_PATH.'gallery.php');
 require_once(UXI_SHORTCODES_PATH.'grid.php');
 //require_once(UXI_SHORTCODES_PATH.'s-current-url.php');
 require_once(UXI_SHORTCODES_PATH.'video.php');
